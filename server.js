f83c3bfdbd44fd921b66ca1711328229c0407b2e
Ben Olsen <olsenben1992@gmail.com>
2020-04-02 15:54:11 -0700

    tracker working

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ app.use(express.json());
 //changed server
 app.use(express.static("public"));
 app.use(router);
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds053380.mlab.com:53380/heroku_61vbc4m2, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://user:password1@ds053380.mlab.com:53380/heroku_61vbc4m2",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
 
 app.listen(PORT, () => {
   console.log("app is running on port " + PORT);
